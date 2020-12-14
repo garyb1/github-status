18bba50ce050b0702491df84c26b3101566a0f27
Gary Byrne <garyb1@live.ie>
2020-12-14 11:56:58 +0000

    clean up and loading

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,37 @@
 <template>
-  <div>
-    <header>
-        <img alt="Github logo" src="./assets/github.svg">
-        <h1>Github Status</h1>
-        <p>Last Updated <span>{{`${updated !== 'unknown' ? new Date(updated).toUTCString() : 'N/A'}`}}</span></p>
-    </header>
-    <main>
-      <ul role="list">
-        <li v-for="component in components" :key="component.id" :class="component.status">
-          <div>
-            <h2>{{component.name}}</h2>
-            <span>{{component.status}}</span>
-          </div>
-          <p>{{component.description}}</p>
-        </li>
-      </ul>
-    </main>
-    <footer>
-      <p :class="status.class">{{status.name}}</p>
-    </footer>
-  </div>
-
+  <header>
+      <img alt="Github logo" src="./assets/github.svg">
+      <h1>Github Status</h1>
+      <p>Last Updated <span>{{`${updated !== 'unknown' ? new Date(updated).toUTCString() : 'N/A'}`}}</span></p>
+  </header>
+  <LoadingBar v-show="isLoading" />
+  <main v-show="components">
+    <ul role="list">
+      <li v-for="component in components" :key="component.id" :class="component.status">
+        <div>
+          <h2>{{component.name}}</h2>
+          <span>{{component.status}}</span>
+        </div>
+        <p>{{component.description}}</p>
+      </li>
+    </ul>
+  </main>
+  <footer v-show="components">
+    <p :class="status.class">{{status.name}}</p>
+  </footer>
 </template>
 
 <script>
-
+import LoadingBar from "./components/LoadingBar";
 export default {
   name: 'App',
+  components: {
+    LoadingBar
+  },
   data: function() {
     return {
-      components: [],
+      components: null,
+      isLoading: true,
       status: {
         name: 'unknown'
       },
@@ -39,35 +41,39 @@ export default {
   mounted() {
     const STATUS_API = 'https://kctbh9vrtdwd.statuspage.io/api/v2/summary.json';
     fetch(STATUS_API).then(res => res.json()).then(response => {
-        this.components = response.components.filter((_, i) => i !== 3);
+        this.components = response.components.filter((_, i) => i !== 3); //remove redundant component
         this.status = {
           name: response.status.description,
           class: response.status.description.split(' ').join('_').toLowerCase()
         };
         this.updated = response.page['updated_at'];
+        this.isLoading = false;
     });
   }
 }
 </script>
 
 <style lang="scss">
-
 body {
   font-family: 'Inter', sans-serif;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  padding: 2rem;
   background-color: #F5F7F8;
   font-size: 16px;
   max-width: 1300px;
+}
 
+#app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100vw;
+  min-height: 100vh;
+  position: relative;
+  padding: 2rem;
   @media(max-width: 700px) {
     padding: 0.5rem;
     justify-content: flex-start;
   }
-
 }
 
 header {
@@ -76,6 +82,7 @@ header {
   justify-content: center;
   align-items: center;
   text-align: center;
+  margin-top: 3rem;
 
   img {
     width: 40px;
@@ -142,5 +149,14 @@ footer p {
 .all_systems_operational {
   background: #2DAF52;
   color: white;
+}
+
+.loading-spinner {
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  left: 50%;
+  top: 20%;
+  transform: translate(-50%, -50%);
 }
 </style>
